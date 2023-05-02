3305f8f0aea0a55113eb9a5639c9bdeb171207d8
sukhdeepsingh9 <sukhibangar@yahoo.com>
2023-05-01 21:22:27 -0400

    Initial commit

---
+++ src/test/java/edu/learn/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.learn.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
